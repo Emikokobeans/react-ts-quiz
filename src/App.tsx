592cc002222dfc23628e2b998bf78cd86c238219
Emi <e.wong@hotmail.co.uk>
2021-10-11 09:22:49 +0100

    add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { userInfo } from 'os';
 import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
-import { fetchQuizQuestions, Difficulty } from './API';
+import { QuestionState, fetchQuizQuestions, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 10;
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
